02b84779f2c606f583484213ac4aa861568b2094
Juancho <33juanpintos33@gmail.com>
2024-06-12 20:22:38 -0300

    Agregar archivos de proyecto.

---
+++ APIS consumir/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIS_consumir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
