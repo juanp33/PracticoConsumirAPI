035349273d0dc803b923cb8f24b05f3397af85fc
Juancho <33juanpintos33@gmail.com>
2024-06-12 21:39:08 -0300

    Agregue apis

---
+++ APIS consumir/ListaPaises.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+namespace Api
+{
+    public class ListaPaises
+    {
+        [JsonProperty("success", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Success { get; set; }
+
+        [JsonProperty("result", NullValueHandling = NullValueHandling.Ignore)]
+        public Result[] Result { get; set; }
+    }
+
+    public partial class Result
+    {
+        [JsonProperty("country_key", NullValueHandling = NullValueHandling.Ignore)]
+        public string CountryKey { get; set; }
+
+        [JsonProperty("country_name", NullValueHandling = NullValueHandling.Ignore)]
+        public string CountryName { get; set; }
+
+        [JsonProperty("country_iso2", NullValueHandling = NullValueHandling.Ignore)]
+        public string CountryIso2 { get; set; }
+
+        [JsonProperty("country_logo", NullValueHandling = NullValueHandling.Ignore)]
+        public Uri CountryLogo { get; set; }
+    }
+}
+
+
+
--- APIS consumir/Program.cs
+++ APIS consumir/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Api;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,29 @@ namespace APIS_consumir
     {
         static void Main(string[] args)
         {
+            
+           
+            var client = new RestClient("https://apiv2.allsportsapi.com/");
+            var request = new RestRequest("football?met=Countries&APIkey=651e021043af5ae3d5d7658a7cf514502022b267825a18dda55779b977341430", Method.Get);
+            RestResponse response =  client.Execute(request);
+            if (response.IsSuccessStatusCode)
+            {
+             ListaPaises misPaises=   JsonConvert.DeserializeObject<ListaPaises>(response.Content);
+            }
+            else
+            {
+                Console.WriteLine(response.Content);
+            }
+            Console.ReadLine();
+
         }
+
+
+
+       
     }
+
 }
+    
+
+
